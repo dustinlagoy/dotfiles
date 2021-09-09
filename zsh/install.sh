c4c1f20860251cc5743524006af08d149551f58e
Dustin Lagoy <dustin@lagoy.org>
2021-09-09 08:41:47 -0700

    install powerlevel10k with zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,3 +2,4 @@
 ln -s "$(pwd)/zshrc" "$HOME/.zshrc"
 ln -s "$(pwd)/zprofile" "$HOME/.zprofile"
 ln -s "$(pwd)/p10k.zsh" "$HOME/.p10k.zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
