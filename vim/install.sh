d3aa8406a49fc4c7162188ad164f35e88f145f8f
Dustin Lagoy <dustin.lagoy@nrc-cnrc.gc.ca>
2022-10-04 10:59:25 -0700

    add docker helpers

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s "$(pwd)/vimrc" "$HOME/.vimrc"
 ln -s "$(pwd)/vimprofile" "$HOME/.vimprofile"
