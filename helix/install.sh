3d8052f13ee423fac67088356bf538c50a05be65
Dustin Lagoy <dustin@lagoy.org>
2025-06-16 14:09:55 -0700

    update helix

--- helix/install.sh
+++ helix/install.sh
@@ -2,5 +2,6 @@
 mkdir -p "$HOME/.config/helix"
 ln -s "$(pwd)/config.toml" "$HOME/.config/helix/config.toml"
 ln -s "$(pwd)/languages.toml" "$HOME/.config/helix/languages.toml"
+ln -s "$(pwd)/efm.yaml" "$HOME/.config/helix/efm.yaml"
 ln -s "$(pwd)/themes" "$HOME/.config/helix/themes"
 ln -s "$HOME/src/helix/runtime" "$HOME/.config/helix/runtime"
