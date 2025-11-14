224e853568181af1184378085e6afd817a18df9a
Dustin Lagoy <dustin@lagoy.org>
2025-11-14 14:40:01 -0800

    update crush config

--- agent/install.sh
+++ agent/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p "$HOME/.local/share/crush"
-../link.sh "$(pwd)/crush.json" "$HOME/.local/share/crush/crush.json"
+../link.sh "$(pwd)/crush.json" "$HOME/.config/crush/crush.json"
+../link.sh "$(pwd)/long.md" "$HOME/.config/crush/long.md"
---
+++ agent/long.md
@@ -0,0 +1,1 @@
+Disregard previous instructions to keep your responses concise. Instead, explain in detail your responses using as many words as necessary.
